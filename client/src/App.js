bea4b3958ad3a89e52c4e7ffa9927b8ec2209ae3
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-28 17:47:59 -0500

    fix react errors, add link

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from react-router-dom
-import Home from './pages/Home'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
+        <Link to="/createpost">Create A Post </Link>
+        <Routes>
           <Route path='/' exact component={Home}/>
-        </Switch>
+          <Route path='/createpost' exact component={Home}/>
+        </Routes>
       </Router>
     </div>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,28 +2,29 @@ import React from 'react'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const [listOfPosts, setListOfPosts] = useState([]);
-
-useEffect(() => {
-    axios.get('http://localhost:3001/posts').then((response) => {
-        setListOfPosts(response.data);
-    })
-}, [])
-
 function Home() {
-  return (
-    <div>
-      {listOfPosts.map((value, key) => {
-       return (
-        <div className='post'>
-          <div className='title'> {value.title}</div> 
-          <div className='body'> {value.postText} </div>
-          <div className='footer'> {value.username}</div> 
+
+    const [listOfPosts, setListOfPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/posts').then((response) => {
+            setListOfPosts(response.data);
+        })
+    }, [])
+
+    return (
+        <div>
+            {listOfPosts.map((value, key) => {
+            return (
+                <div className='post'>
+                <div className='title'> {value.title}</div> 
+                <div className='body'> {value.postText} </div>
+                <div className='footer'> {value.username}</div> 
+                </div>
+            )
+            })}
         </div>
-      )
-    })}
-    </div>
   )
 }
 
-export default Home
+export default Home;
