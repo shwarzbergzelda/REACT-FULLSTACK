75813691008a0629199f22a94a6b75b8d43e8669
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-28 17:40:36 -0500

    add home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from react-router-dom
+import Home from './pages/Home'
 function App() {
-
-  
-
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
