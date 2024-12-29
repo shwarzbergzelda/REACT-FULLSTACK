75f29a7642af45c57d72558c6a1637da472019fe
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-29 09:34:43 -0500

    finish comment section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,6 +18,7 @@ function Home() {
       {listOfPosts.map((value, key) => {
         return (
           <div
+            key={key}
             className="post"
             onClick={() => {
               navigate(`post/${value.id}`);
--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,12 +5,31 @@ import axios from "axios";
 function Post() {
   let { id } = useParams();
   const [postObject, setPostObject] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
     axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
       setPostObject(response.data);
     });
-  });
+
+    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      setComments(response.data);
+    });
+  }, []);
+
+  const addComment = () => {
+    axios
+      .post("http://localhost:3001/comments", {
+        commentBody: newComment,
+        PostId: id,
+      })
+      .then((response) => {
+        const commentToAdd = { commentBody: newComment };
+        setComments([...comments, commentToAdd]);
+        setNewComment("");
+      });
+  };
 
   return (
     <div className="postPage">
@@ -23,10 +42,25 @@ function Post() {
       </div>
       <div className="rightSide">
         <div className="addCommentContainer">
-          <input type="text" placeholder="Comment..." />
-          <button> Add Comment </button>
+          <input
+            type="text"
+            placeholder="Comment..."
+            value={newComment}
+            onChange={(event) => {
+              setNewComment(event.target.value);
+            }}
+          />
+          <button onClick={addComment}> Add Comment </button>
         </div>
-        <div className="listOfComments"></div>
+        <div className="listOfComments">
+          {comments.map((comment, key) => {
+            return (
+              <div key={key} className="comment">
+                {comment.commentBody}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
