aff47bb314c9e62fed1aee9f8ae67b62b1a48ea4
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-29 08:46:08 -0500

    make comments endpoint

--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { Comments } = require("../models");
 
-
-
-
+router.get("/:postId", async (req, res) => {
+  const postId = req.params.postId;
+  const comments = await Comments.findAll({ where: { PostId: postId } });
+  res.json(comments);
+});
 
 module.exports = router;
