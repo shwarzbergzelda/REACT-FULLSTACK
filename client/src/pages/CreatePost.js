5807da98deb2202f04b0c3aa053e732c383c188c
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-28 19:22:15 -0500

    add initialValues, onSubmit

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,36 +2,45 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function CreatePost() {
-  return (
-    <div className='createPostPage'>
-        <Formik>
-            <Form className='formContainer'>
-                <label>Title: </label>
-                <Field
-                    id='inputCreatePost'
-                    name='title'
-                    placeholder='Ex. Title...'
-                />
 
-                <label>Post: </label>
-                <Field
-                    id='inputCreatePost'
-                    name='postText'
-                    placeholder='Ex. Post...'
-                />
+    const initialValues = {
+        title: '',
+        postText: '',
+        username: '',
+    }
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+    return (
+        <div className='createPostPage'>
+            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                <Form className='formContainer'>
+                    <label>Title: </label>
+                    <Field
+                        id='inputCreatePost'
+                        name='title'
+                        placeholder='Ex. Title...'
+                    />
 
-                <label>Username: </label>
-                <Field
-                    id='inputCreatePost'
-                    name='username'
-                    placeholder='Ex. John123...'
-                />
+                    <label>Post: </label>
+                    <Field
+                        id='inputCreatePost'
+                        name='postText'
+                        placeholder='Ex. Post...'
+                    />
 
-                <button type='submit'>Create Post</button>
-            </Form>
-        </Formik>
-    </div>
-  )
+                    <label>Username: </label>
+                    <Field
+                        id='inputCreatePost'
+                        name='username'
+                        placeholder='Ex. John123...'
+                    />
+
+                    <button type='submit'>Create Post</button>
+                </Form>
+            </Formik>
+        </div>
+    )
 }
 
 export default CreatePost
