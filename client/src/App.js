26d4d623a06b7386a5bc1448b1423f25ef1e0e69
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-28 19:47:23 -0500

    add styling to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import CreatePost from './pages/CreatePost'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/createpost">Create A Post </Link>
-        <Link to="/">Home Page </Link>
+        <div className="navbar">
+          <Link to="/createpost">Create A Post </Link>
+          <Link to="/">Home Page </Link>
+        </div>
         <Routes>
-          <Route path='/' exact element={< Home />}/>
-          <Route path='/createpost' exact element={<CreatePost />}/>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/createpost" exact element={<CreatePost />} />
         </Routes>
       </Router>
     </div>
