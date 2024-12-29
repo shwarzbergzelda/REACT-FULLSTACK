5b4dbfefc0ff815932d078a5ae5f00fb5805954f
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-28 19:12:49 -0500

    finish skeleton of create post form

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -10,8 +10,24 @@ function CreatePost() {
                 <Field
                     id='inputCreatePost'
                     name='title'
-                    placeholder='Ex. Title'
+                    placeholder='Ex. Title...'
                 />
+
+                <label>Post: </label>
+                <Field
+                    id='inputCreatePost'
+                    name='postText'
+                    placeholder='Ex. Post...'
+                />
+
+                <label>Username: </label>
+                <Field
+                    id='inputCreatePost'
+                    name='username'
+                    placeholder='Ex. John123...'
+                />
+
+                <button type='submit'>Create Post</button>
             </Form>
         </Formik>
     </div>
