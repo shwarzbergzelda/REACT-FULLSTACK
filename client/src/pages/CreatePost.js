0d2baf69382c263887f1faa97c1a94db651a6aad
shwarzbergzelda <shwarzbergzelda@gmail.com>
2024-12-28 19:25:41 -0500

    add validation scheme using yup

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup'
 
 function CreatePost() {
 
@@ -7,10 +8,16 @@ function CreatePost() {
         title: '',
         postText: '',
         username: '',
-    }
+    };
+
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required(),
+        postText: Yup.string().required(),
+        username: Yup.string().min(3).max(15).required()
+    })
     const onSubmit = (data) => {
         console.log(data);
-    }
+    };
     return (
         <div className='createPostPage'>
             <Formik initialValues={initialValues} onSubmit={onSubmit}>
